668875684fa31895c16c70fe8efdb6c77c01aba2
Jesaja Hur <jesaja.hur@web.de>
2024-03-04 20:18:23 +0100

    Initiated project for CS50P

---
+++ project.py
@@ -0,0 +1,24 @@
+import sys
+
+def main():
+    ...
+
+    # Implement a menu system for the user to use
+        # ---User management : adding, deleting, selecting user (Only for final version)---
+    
+        # Showing history of expenses for given user
+        # Adding, Deleting an expense
+    
+    # Implementing classes
+        # Class for user
+        # Class for expense
+    
+    # Implement a file structure (JSON, CSV)
+
+    # Configure 2 functions (requirements for CS50P)
+        # Show history
+        #     
+
+
+if __name__ == "__main__":
+    main()
