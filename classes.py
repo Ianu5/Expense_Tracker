06ebfe660769ad5af2c8ad47b9b8323ecc0dbc3f
Jesaja Hur <j.hur@iqul.de>
2024-03-05 16:45:50 +0100

    Added main function, menu, adding expense and file management

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Expense:
+    '''This class represents an Expense'''
+    def __init__(self, amount, category, date):
+        self.amount = amount
+        self.category = category
+        self.date = date
+        
--- project.py
+++ project.py
@@ -1,24 +1,52 @@
 import sys
+import os
+import json
+from classes import Expense
 
 def main():
-    ...
 
-    # Implement a menu system for the user to use
-        # ---User management : adding, deleting, selecting user (Only for final version)---
-    
-        # Showing history of expenses for given user
-        # Adding, Deleting an expense
-    
-    # Implementing classes
-        # Class for user
-        # Class for expense
-    
-    # Implement a file structure (JSON, CSV)
+    if os.path.exists('expenses.json'):
+        expenses = get_data_from_existing_file()
+    else:
+        expenses = []
 
-    # Configure 2 functions (requirements for CS50P)
-        # Show history
-        #     
 
+    while True:
+        show_menu()
+
+        response = input("Your choice: ")
+        if response == "1":
+            amount = input("Amount spent: ")
+            category = input("Reason for spending: ")
+            date = input("Date of expense: ")
+            expense = Expense(amount, category, date)
+            expenses.append(expense.__dict__)
+
+        if response == "2":
+            print("\n-----------------------\nYour expenses\n")
+            for expense in expenses:
+                print(f"date: {expense["date"]}\namount: {expense["amount"]}\n")
+            print("-----------------------")
+                
+        if response == "3":
+            with open ('expenses.json', 'w') as file:
+                json.dump(expenses, file, indent=2)
+            break
+
+
+
+def show_menu():
+    print("""
+1. Add an expense
+2. Show expense history
+3. Quit
+            """)
+    
+def get_data_from_existing_file():
+    with open('expenses.json', 'r') as file:
+            expenses = json.load(file)
+    return expenses
+    
 
 if __name__ == "__main__":
     main()
